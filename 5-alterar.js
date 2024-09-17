e681e7f6b46fdc1ddb4f285bf97cf8b70c0202b1
beatrizoliveir <147163128+beatrizoliveir@users.noreply.github.com>
2024-09-17 11:05:40 -0300

    Update 5-alterar.js

---
+++ 5-alterar.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: "Biah",
+    profissao: "Engenharia",
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.telefone);//undefined - 
+pessoa.telefone = "11 123456789"; // inclusão telefone
+console.log(pessoa.telefone);
+pessoa.nome = "Biah Linda"; // alteração nome
+console.log(pessoa);
+const novaPessoa = {
+    nome: "Beatriz",
+};
